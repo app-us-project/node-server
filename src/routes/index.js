dfc56ab853dbb3ec9687a89ce8913f02a2e4393a
god9599 <59276134+god9599@users.noreply.github.com>
2021-01-27 21:31:00 +0900

    Add swagger routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { swaggerUi, specs } = require('../lib/swagger');
+
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 router.get('/', (req, res) => {
   res.send('ok');
