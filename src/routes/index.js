cf102e73404f76682dc11b34cac50ac82a937268
god9599 <59276134+god9599@users.noreply.github.com>
2021-02-09 10:29:40 +0900

    Api routing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api/v1');
 
 router.get('/', (req, res) => {
   res.send('ok');
 });
+
+router.use('/api', apiRouter);
 
 module.exports = router;
