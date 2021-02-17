b84bb634f409bf4c68cd646193308aa67c06417b
god9599 <59276134+god9599@users.noreply.github.com>
2021-02-18 00:42:22 +0900

    Update product.router.js
    Add product routing

--- src/routes/api/v1/product.router.js
+++ src/routes/api/v1/product.router.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { postProduct, deleteProduct } = require('../../../controllers/product.controller.js');
+const { postProduct, deleteProduct, getAllProduct, updateProduct, getProduct } = require('../../../controllers/product.controller.js');
 const upload = require('../../../modules/multer');
 
 router.post('/', upload.single('image'), postProduct); //POST api/product
 router.delete('/:id', deleteProduct); //DELETE api/product/:id
+router.get('/', getAllProduct); //GET api/product
+router.put('/:id', updateProduct); //PUT api/product/:id
+router.get('/:id', getProduct); //GET api/product/:id
 
 module.exports = router;
