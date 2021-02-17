c005ce2a4a1898febde85602813e0b4eaa5158c3
god9599 <59276134+god9599@users.noreply.github.com>
2021-02-17 22:10:34 +0900

    Update connect option

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 const env = process.env;
 
-const development = {
+const local = {
   username: env.MYSQL_USERNAME,
   password: env.MYSQL_PASSWORD,
   database: env.MYSQL_DATABASE,
   host: env.MYSQL_HOST,
   dialect: env.MYSQL_DIALECT,
   port: env.MYSQL_PORT
+};
+
+const development = {
+  host: env.AWS_HOST,
+  username: env.AWS_USERNAME,
+  password: env.AWS_PASSWORD,
+  database: env.AWS_DATABASE,
+  PORT: env.AWS_PORT,
+  dialect: env.AWS_DIALECT,
 };
 
 const production = {
@@ -28,4 +37,4 @@ const test = {
   port: env.MYSQL_PORT
 };
 
-module.exports = { development, production, test };
+module.exports = { development, production, test, local };
