988980c9b775268ef4f06af7caf95445d7ed1147
god9599 <59276134+god9599@users.noreply.github.com>
2021-02-24 02:31:24 +0900

    contentType 설정 코드 추가

--- src/modules/multer.js
+++ src/modules/multer.js
@@ -11,11 +11,12 @@ const s3 = new aws.S3({
   region: 'ap-northeast-2'
 });
 
-const upload = multer({
+const upload = multer({     
   storage: multerS3({
     s3: s3,
     bucket: env.AWS_BUCKET_NAME,
     acl: 'public-read',
+    contentType : multerS3.AUTO_CONTENT_TYPE,
     key: function(req, file, cb){
       cb(null, file.originalname);
     }
