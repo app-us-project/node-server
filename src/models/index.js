6e7ccf5bbe4f5600f240b05903e39f6b856f5102
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-04 00:21:59 +0900

    테이블 연결 추가

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development'; //배포할 때 production으로 설정
+const env = process.env.NODE_ENV || 'local'; //배포할 때 production으로 설정
 const config = require('../config/index')[env];
 const db = {};
 
 const Product = require('./product');
+const Image = require('./image');
+const Cart = require('./cart');
+const Category = require('./category');
 
 const sequelize = new Sequelize(
   config.database, 
@@ -17,8 +20,18 @@ const sequelize = new Sequelize(
 db.sequelize = sequelize;
 
 db.Product = Product;
+db.Image = Image;
+db.Cart = Cart;
+db.Category = Category;
+
 Product.init(sequelize);
+Image.init(sequelize);
+Cart.init(sequelize);
+Category.init(sequelize);
 
 Product.associate(db);
+Image.associate(db);
+Cart.associate(db);
+Category.associate(db);
 
 module.exports = db;
