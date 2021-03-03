e60a405f23fa5720560c63a23fcd1f93e42c6129
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-04 00:21:17 +0900

    라우터 추가

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const productRouter = require('./product.router');
+const imageRouter = require('./image.router');
 
 router.use('/product', productRouter);
+router.use('/product', imageRouter);
 
 module.exports = router;
--- src/routes/api/v1/product.router.js
+++ src/routes/api/v1/product.router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { postProduct, deleteProduct, getAllProduct, updateProduct, getProduct } = require('../../../controllers/product.controller.js');
-const upload = require('../../../modules/multer');
 
-router.post('/', upload.single('image'), postProduct); //POST api/product
+// 관리자
+router.post('/', postProduct); //POST api/product
 router.delete('/:id', deleteProduct); //DELETE api/product/:id
-router.get('/', getAllProduct); //GET api/product
 router.put('/:id', updateProduct); //PUT api/product/:id
-router.get('/:id', getProduct); //GET api/product/:id
+
+router.get('/', getAllProduct); //GET api/product
+router.get('/:title', getProduct); //GET api/product/:name
 
 module.exports = router;
