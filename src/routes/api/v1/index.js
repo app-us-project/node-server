c02ba2259faccb92187fb6e69971347360556fc3
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-14 01:25:15 +0900

    찜 라우터 추가

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const productRouter = require('./product.router');
 const imageRouter = require('./image.router');
+const wishListRouter = require('./wishList.router');
 
 router.use('/product', productRouter);
 router.use('/product', imageRouter);
+router.use('/', wishListRouter);
 
 module.exports = router;
