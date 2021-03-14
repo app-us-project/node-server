9662f8e63f97aa8947ae266e056052794fd03a15
DongWooE <ehddn977@naver.com>
2021-03-14 17:30:16 +0900

    아직 만들어지지않은 wishcontroller 주석 처리

--- src/routes/api/v1/wishList.router.js
+++ src/routes/api/v1/wishList.router.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {addWishList, getAllWishList, deleteWishList} = require('../../../controllers/wishList.controller');
+//const {addWishList, getAllWishList, deleteWishList} = require('../../../controllers/wishList.controller');
 
-router.post('/:productId/wishList', addWishList);
-router.get('/wishList/', getAllWishList);
-router.delete('/wishList/:wishListId', deleteWishList);
+//router.post('/:productId/wishList', addWishList);
+//router.get('/wishList/', getAllWishList);
+//router.delete('/wishList/:wishListId', deleteWishList);
 
 module.exports  = router;
