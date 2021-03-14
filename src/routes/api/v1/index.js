3eb570172333b1674d5acb4d8840aa259098fbad
DongWooE <ehddn977@naver.com>
2021-03-14 16:37:27 +0900

    나머지 부분 merging

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const orderItemRouter = require('./orderItem.router');
 const orderRouter = require('./order.router');
 
 router.use('/order', orderRouter);
 router.use('/orderItem', orderItemRouter);
 
-module.exports = router;
-=======
 const productRouter = require('./product.router');
 const imageRouter = require('./image.router');
 const wishListRouter = require('./wishList.router');
@@ -18,4 +15,3 @@ router.use('/product', imageRouter);
 router.use('/', wishListRouter);
 
 module.exports = router;
->>>>>>> eac178c4e49e775abf3b7a59062b77737e5502c0
