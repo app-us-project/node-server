669f7dc0a55375d38a9c14af4573f412c3ad5150
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-15 15:44:03 +0900

    장바구니에 추가 기능

---
+++ src/controllers/cart.controller.js
@@ -0,0 +1,31 @@
+const Product = require('../models/product');
+const Image = require('../models/image');
+const Cart = require('../models/cart');
+
+const addCart = async(req, res, next) => {
+  try {
+    const productId = req.params.productId;
+    const auth = req.headers.authentication;
+    const quantity = req.body.quantity;
+    const product = await Product.findByPk(productId);
+    const image = await Image.findOne({
+      where: {ProductId: productId},
+    })
+    let priceTemp = quantity * product.price;
+    const cart = await Cart.create({
+      title : product.title,
+      userId: auth,
+      price : product.price,
+      totalPrice : priceTemp,
+      quantity: quantity,
+      ProductId: productId,
+      ImageId: image.id 
+  });
+  res.json({data: cart});
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+} 
+
+module.exports = {addCart};
