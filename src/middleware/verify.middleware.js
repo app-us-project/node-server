636c91ae8ed5b16bd16c9d13f2c5ecaa5f4e861b
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-17 11:06:52 +0900

    JWT Verify 미들웨어 추가

---
+++ src/middleware/verify.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = async(req, res, next) =>{
+  try{
+    const authentication = req.headers.authentication;
+    if(!authentication){
+      res.status(400).json({message: 'Bad Request, no authentication'});
+    }
+    jwt.verify(authentication, process.env.JWT_SECRETKEY, (error, decoded) => {
+      if(error){
+        res.status(401).json({message: "Unauthorization"});
+      }
+    console.log(decoded.jti);
+    req.auth = {
+      id: decoded.jti
+    }
+    next();
+    })
+  }catch(error){
+    res.status(401).json({message: "key experience error"});
+  }
+}
+
+module.exports = {verifyToken};
