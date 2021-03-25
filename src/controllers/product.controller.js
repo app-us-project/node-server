a86d698d478a6e56595ec9832cbba612cc56ff75
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-25 23:07:09 +0900

    상품 설명 컬럼 삭제 후 적용

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,86 +1,93 @@
-const Product = require('../models/product');
-const Image = require('../models/image');
+const Product = require("../models/product");
+const Image = require("../models/image");
 
-const postProduct = async(req, res, next) => {
+const postProduct = async (req, res, next) => {
   try {
     await Product.create({
       title: req.body.title,
-      description: body.description,
-      price: req.body.price
+      price: req.body.price,
     });
     res.status(201).send("Success create product");
   } catch (error) {
     console.error(error);
     next(error);
   }
-}
+};
 
-const deleteProduct = async(req, res, next) => {
+const deleteProduct = async (req, res, next) => {
   try {
     await Product.destroy({
-      where: {id : req.params.id},
+      where: { id: req.params.id },
     });
     res.status(201).send("Success delete product");
   } catch (error) {
     console.error(error);
     next(error);
   }
-}
+};
 
-const getAllProduct = async(req, res, next) => {
+const getAllProduct = async (req, res, next) => {
   try {
     product = await Product.findAll({
       include: {
         model: Image,
-        attributes: ['imageUrl', 'id'],
-        order: [['id', 'ASC']]
-      }
+        attributes: ["imageUrl", "id"],
+        order: [["id", "ASC"]],
+      },
     });
-    if(!product){
-      throw Error('no product');
+    if (!product) {
+      throw Error("no product");
     }
-    res.status(201).json({data: product});
+    res.status(201).json({ data: product });
   } catch (error) {
     console.error(error);
     next(error);
   }
-}
+};
 
-const getProduct = async(req, res, next) => {
+const getProduct = async (req, res, next) => {
   try {
     const product = await Product.findOne({
-      where: {title: req.params.title},
+      where: { title: req.params.title },
       include: {
         model: Image,
-        attributes: ['imageUrl', 'id'],
+        attributes: ["imageUrl", "id"],
       },
     });
-    if(!product){
-      throw Error('no product');
+    if (!product) {
+      throw Error("no product");
     }
     res.status(201).send(product);
   } catch (error) {
     console.error(error);
     next(error);
   }
-}
+};
 
-const updateProduct = async(req, res, next) => {
+const updateProduct = async (req, res, next) => {
   try {
     const body = req.body;
     const productId = req.params.id;
-    await Product.update({
-      title: body.title,
-      description: body.description,
-      price: body.price,
-    }, {
-      where: {id: productId}
-    });
-    res.status(200).send('success');
+    await Product.update(
+      {
+        title: body.title,
+        price: body.price,
+      },
+      {
+        where: { id: productId },
+      }
+    );
+    res.status(200).send("success");
   } catch (error) {
     console.error(error);
     next(error);
   }
-}
+};
 
-module.exports = { postProduct, deleteProduct, getAllProduct, updateProduct, getProduct };
+module.exports = {
+  postProduct,
+  deleteProduct,
+  getAllProduct,
+  updateProduct,
+  getProduct,
+};
--- src/models/product.js
+++ src/models/product.js
@@ -1,34 +1,33 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 module.exports = class Product extends Sequelize.Model {
-  static init(sequelize){
-    return super.init({
-      title: {
-        type: Sequelize.STRING(20),
-        allowNull: false,
-        unique: true,
+  static init(sequelize) {
+    return super.init(
+      {
+        title: {
+          type: Sequelize.STRING(20),
+          allowNull: false,
+          unique: true,
+        },
+        price: {
+          type: Sequelize.INTEGER.UNSIGNED,
+          allowNull: false,
+        },
       },
-      description: {
-        type: Sequelize.STRING(100),
-        allowNull: false,
-      },
-      price: {
-        type: Sequelize.INTEGER.UNSIGNED,
-        allowNull: false,
+      {
+        sequelize,
+        timestamps: false,
+        modelName: "Product",
+        tableName: "Products",
+        charset: "utf8",
+        collate: "utf8_general_ci",
       }
-    }, {
-      sequelize,
-      timestamps: false,
-      modelName: 'Product',
-      tableName: 'Products',
-      charset: 'utf8',
-      collate: 'utf8_general_ci',
-    });
+    );
   }
   static associate(db) {
     db.Product.hasMany(db.Image);
     db.Product.hasMany(db.Cart);
     db.Product.hasMany(db.wishList);
-    db.Product.belongsToMany(db.Category, { through: 'ProductCategory' });
+    db.Product.belongsToMany(db.Category, { through: "ProductCategory" });
   }
 };
