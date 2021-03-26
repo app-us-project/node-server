538502f996dae2df07760645832a545d6d70517c
god9599 <59276134+god9599@users.noreply.github.com>
2021-03-27 03:12:03 +0900

    Style: 코드 가독성

--- src/routes/api/v1/cart.router.js
+++ src/routes/api/v1/cart.router.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getAllCarts, addCart, deleteCart} = require('../../../controllers/cart.controller.js');
+const {
+  getAllCarts,
+  addCart,
+  deleteCart,
+} = require("../../../controllers/cart.controller.js");
 
-router.post('/:productId/cart', addCart);     
-router.get('/cart', getAllCarts);
-router.delete('/cart/:cartId', deleteCart);
+router.get("/cart", getAllCarts);
+router.post("/:productId/cart", addCart);
+router.delete("/cart/:cartId", deleteCart);
 
-module.exports = router; 
+module.exports = router;
