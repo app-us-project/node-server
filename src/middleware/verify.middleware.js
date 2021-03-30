9128be148274267d64ad4b07d2dfcdaed2e4311d
icarus8050 <icarus8050@naver.com>
2021-03-30 23:32:40 +0900

    jwt 토큰이 존재할 경우에만 auth 값을 설정하도록 미들웨어 수정

--- src/middleware/verify.middleware.js
+++ src/middleware/verify.middleware.js
@@ -1,25 +1,25 @@
 const jwt = require('jsonwebtoken');
 
-const verifyToken = async(req, res, next) =>{
-  try{
-    const authentication = req.headers.authentication;
-    if(!authentication){
-      res.status(400).json({message: 'Bad Request, no authentication'});
+const verifyToken = async (req, res, next) => {
+    try {
+        const authentication = req.headers.authentication;
+        if (authentication) {
+            jwt.verify(authentication, process.env.JWT_SECRETKEY, (error, decoded) => {
+                if (error) {
+                    res.status(401).json({message: "Unauthorization"});
+                    return;
+                }
+                console.log(decoded.jti);
+                req.auth = {
+                    id: decoded.jti
+                }
+            })
+        }
+    } catch (error) {
+        res.status(401).json({message: "key experience error"});
     }
-    jwt.verify(authentication, process.env.JWT_SECRETKEY, (error, decoded) => {
-      if(error){
-        res.status(401).json({message: "Unauthorization"});
-        return;
-      }
-    console.log(decoded.jti);
-    req.auth = {
-      id: decoded.jti
-    }
+
     next();
-    })
-  }catch(error){
-    res.status(401).json({message: "key experience error"});
-  }
 }
 
 module.exports = {verifyToken};
